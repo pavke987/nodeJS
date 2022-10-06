6d6d8250e51d857acb47d0ef788e451efbc0c507
pavke987 <pavke987@gmail.com>
2022-10-06 12:26:30 +0200

    on line shop update

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+const express = require("express");
 
-const http = require('http');
-const routes = require('./routes');
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
 
-server.listen(3000);
+app.set("view engine", "ejs");
+app.set("views", "views");
 
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found!", pageText: '404 ERROR' });
+});
+
+app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+const express = require("express");
+const rootDir = require("../util/path");
+const router = express.Router();
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    products: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCss: true,
+  });
+});
+
+module.exports = router;
